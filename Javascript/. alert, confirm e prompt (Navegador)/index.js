9d342f3c66e8f9a40c5f4609c3af96f738743b26
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-02-26 21:18:03 -0300

    Exercicio concluido
    Nesse exercício foi solicitado que pegasse 2 números pelo prompt e transformasse eles em  Number para realizar a soma deles e devolver pelo alert uma mensagem do resultado obtido na soma.

--- Javascript/. alert, confirm e prompt (Navegador)/index.js
+++ Javascript/. alert, confirm e prompt (Navegador)/index.js
@@ -2,8 +2,8 @@ let numero1 = prompt('Digite o primeiro número')
 let numero2 = prompt('Digite o segundo número')
 
 numero1 = Number(numero1)
-numero2 = Number(numero2)
+numero2 = Number(numero2) 
 
 let resultado = numero1 + numero2
 
-alert(`O resultado ${numero1} + ${numero2} é igual a ${resultado}`)
+alert(`O resultado de ${numero1} + ${numero2} é igual a ${resultado}`)
