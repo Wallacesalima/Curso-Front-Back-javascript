60bcbc348d2072e6f5891f1498f12bd1aa97d2f8
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-02-28 21:01:14 -0300

    Criando um arquivo para Math
    Usando Math podemos fazer algumas contas, visto a sua importância fiz um arquivo para possível consulta.

---
+++ Javascript/Math/index.js
@@ -0,0 +1,17 @@
+let num1 = 9.56464
+
+let arredondaParaBaixo = Math.floor(num1)
+let arredondaParaCima = Math.ceil(num1)
+let arredondaParaoMaisProximo = Math.round(num1)
+
+let pegaOnumeroMaisAlto = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9)
+let pegaOnumeroMaisBaixo = Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+let aleatorio = Math.round(Math.random() * (10) + 5)
+
+console.log(arredondaParaBaixo)
+console.log(arredondaParaCima)
+console.log(arredondaParaoMaisProximo)
+console.log(pegaOnumeroMaisAlto)
+console.log(pegaOnumeroMaisBaixo)
+console.log(aleatorio) 
