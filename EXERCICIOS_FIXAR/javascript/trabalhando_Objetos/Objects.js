085155bc15c08cde05f836c12d916c0f3bd3ae8a
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-03-10 20:48:28 -0300

    Resolvendo Exercicio com Objetos
    Manipulando Objeto para fixar o conteúdo.

--- EXERCICIOS_FIXAR/javascript/trabalhando_Objetos/Objects.js
+++ EXERCICIOS_FIXAR/javascript/trabalhando_Objetos/Objects.js
@@ -1,9 +1,21 @@
 // Criando Objeto pessoa
 const pessoa = {
-    nome: "wallace",
+    nome: "Wallace",
     idade: 25,
     cidade: "São Paulo"
 }
 
 // Exibindo o Objeto inicial
 console.log(`Exibindo o Objeto sem modificações:`, pessoa)
+
+// Exibindo somente o Nome Odo objeto pessoa
+console.log(`Nome dentro do objeto pessoa:`, pessoa.nome)
+
+// Mudando a cidade
+pessoa.cidade = "Curitiba"
+
+// Adicionando profissao ao objeto pessoa
+pessoa.profissao = "Eng de Software"
+
+// Exibindo o Objeto modificado
+console.log(`Esse é o Objeto modificado:`, pessoa)
