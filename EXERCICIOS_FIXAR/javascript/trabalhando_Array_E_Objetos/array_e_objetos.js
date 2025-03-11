0e5dfa7a865df5e7cd8f3c717efd7cbe2bfcf3f1
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-03-10 21:13:26 -0300

    Resolvendo exercício de array juntamente com objetos
    Usei Objetos dentro de array e manipulei esses dados para fixar o conteudo.

---
+++ EXERCICIOS_FIXAR/javascript/trabalhando_Array_E_Objetos/array_e_objetos.js
@@ -0,0 +1,40 @@
+// Criando o array pessoas, com 3 objetos (pessoa1, pessoa2 e pessoa3)
+const pessoas = [
+    {
+        nome: "Wallace",
+        idade: 25,
+        cidade: "São Paulo"
+    },
+    {
+        nome: "Andre",
+        idade: 35,
+        cidade: "Bahia"
+    },
+    {
+        nome: "Alfonso",
+        idade: 76,
+        cidade: "Ceara"
+    },
+]
+
+// Exibindo o array pessoas
+console.log(`O array pessoas contém esses dados:`, pessoas)
+
+// Exibindo o nome do objeto pessoa2
+console.log(`Nome do segundo objeto:`, pessoas[1].nome)
+
+// Modificando a idade do objeto pessoa3
+pessoas[2].idade = 57
+
+// Adicionando o Objeto pessoa4 ao array pessoas
+pessoas.push({
+        nome: "Poatan",
+        idade: 34,
+        cidade: "Paraíba"
+})
+
+// Exibindo o array pessoas após mudanças
+console.log(`O array pessoas atualizado contém esses dados:`,pessoas)
+
+
+
