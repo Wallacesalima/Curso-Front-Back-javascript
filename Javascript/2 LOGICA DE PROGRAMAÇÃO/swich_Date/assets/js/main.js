01bd67acb7f1ea94a9532a514dd99e4e45535c8a
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-03-28 21:51:53 -0300

    Adicionando a resolução da atividade
    Usando funções para resolução para que a data e hora sempre atualizem;

--- Javascript/2 LOGICA DE PROGRAMAÇÃO/swich_Date/assets/js/main.js
+++ Javascript/2 LOGICA DE PROGRAMAÇÃO/swich_Date/assets/js/main.js
@@ -1,23 +1,55 @@
-const dataNoHtml = document.querySelectorAll('.dataHtml')
+const dataNoHtml = document.querySelector('.dataHtml')
 
-function zeroAEsquerda (num) {
-    return num >10 ? num : `0${num}`
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`;
 }
 
-function formataData (data) {
-    const dia = zeroAEsquerda(data.getDate())
-    const mes = zeroAEsquerda(data.getMonth() + 1) // Mês começa no zero - janeiro = 0 e dezembro = 11
-    const ano = zeroAEsquerda(data.getFullYear())
-    const hora = zeroAEsquerda(data.getHours())
-    const min = zeroAEsquerda(data.getMinutes())
-    const seg = zeroAEsquerda(data.getSeconds())
-    // const diaSemana = data.getDay() Dia da semana começa em 0 - domindo = 0 e Sábado = 6
-
-
-    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+function getDiaSemanaTexto(diaSemana) {
+    switch (diaSemana) {
+        case 0: return 'Domingo';
+        case 1: return 'Segunda-feira';
+        case 2: return 'Terça-feira';
+        case 3: return 'Quarta-feira';
+        case 4: return 'Quinta-feira';
+        case 5: return 'Sexta-feira';
+        case 6: return 'Sábado';
+        default: return '';
+    }
 }
 
-const data = new Date() 
-const dataBrasil = formataData(data)
+function getMesTexto(mes) {
+    switch (mes) {
+        case 1: return 'Janeiro';
+        case 2: return 'Fevereiro';
+        case 3: return 'Março';
+        case 4: return 'Abril';
+        case 5: return 'Maio';
+        case 6: return 'Junho';
+        case 7: return 'Julho';
+        case 8: return 'Agosto';
+        case 9: return 'Setembro';
+        case 10: return 'Outubro';
+        case 11: return 'Novembro';
+        case 12: return 'Dezembro';
+        default: return '';
+    }
+}
 
-dataNoHtml.innerHTML = `Essa é a data: ${dataBrasil}`
+function main() {
+    const data = new Date();
+
+    const diaSemanaTexto = getDiaSemanaTexto(data.getDay());
+    
+    const mesTexto = getMesTexto(data.getMonth() + 1); // Aqui somamos 1 ao mês
+
+    const dia = data.getDate();
+    const ano = data.getFullYear();
+    const hora = zeroAEsquerda(data.getHours());
+    const mins = zeroAEsquerda(data.getMinutes());
+    
+    dataNoHtml.innerHTML = `${diaSemanaTexto}, ${dia} de ${mesTexto} de ${ano}<br/> ${hora}:${mins}`;
+}
+
+// Atualiza a cada 1 segundo
+setInterval(main, 1000);
+main();
