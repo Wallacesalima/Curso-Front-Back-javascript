a9bce1fbeac78845a226be8efd375d2ce8b767a5
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-04-01 19:51:38 -0300

    Adicionando a div que faltava.

--- Javascript/2 LOGICA DE PROGRAMAÇÃO/for_desestruturação/assets/js/main.js
+++ Javascript/2 LOGICA DE PROGRAMAÇÃO/for_desestruturação/assets/js/main.js
@@ -5,12 +5,14 @@ const elementos = [
     { tag: 'section', texto: 'Frase 4' },
 ]
 
-const section = document.querySelector('.container')
+const container = document.querySelector('.container') // selecionando a classe container
+const div = document.createElement('div') // criando uma div
 
-for (let i = 0; i < elementos.length; i++) { // Agora está correto
+for (let i = 0; i < elementos.length; i++) {
     const { tag, texto } = elementos[i] // Pegando tag e texto
     const elementosHtml = document.createElement(tag) // Criando o elemento
     elementosHtml.textContent = texto // Adicionando o texto
-    section.appendChild(elementosHtml) // Inserindo na página
+    div.appendChild(elementosHtml) // Inserindo na div criada
 }
 
+container.appendChild(div) // adicionando a div dentro do container
