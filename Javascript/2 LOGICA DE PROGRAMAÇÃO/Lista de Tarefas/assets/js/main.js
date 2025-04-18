bb7618c2ab98a323280db38b1010c432440b0832
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-04-18 16:34:34 -0300

    implementa lista de tarefas com persistência em localStorage
    Implementa lista de tarefas com criação, remoção e salvamento no localStorage
    Adiciona funcionalidades para criar tarefas, remover com botão "Apagar" e salvar a lista no localStorage. Também permite adicionar tarefas com Enter ou clique no botão.

---
+++ Javascript/2 LOGICA DE PROGRAMAÇÃO/Lista de Tarefas/assets/js/main.js
@@ -0,0 +1,80 @@
+const inputTarefa = document.querySelector('.input-tarefa')
+const btnTarefa = document.querySelector('.btn-tarefa')
+const tarefas = document.querySelector('.tarefas')
+
+function criarLi() {
+    const li = document.createElement('li')
+    return li
+}
+
+function criarTarefa(textInput) {
+    const li = criarLi()
+    tarefas.appendChild(li)
+    li.innerText = textInput
+    limpaInput()
+    criaBotaoApagar(li)
+    salvarTarefas()
+}
+
+function limpaInput() {
+    inputTarefa.value = ''
+    inputTarefa.focus()
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += '  '
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'Apagar essa tarefa')
+    li.appendChild(botaoApagar)
+}
+
+function apagarTarefa() {
+    document.addEventListener('click', function (e) {
+
+        const el = e.target
+    
+        if (el.classList.contains('apagar')) {
+            el.parentElement.remove()
+            salvarTarefas()
+        }
+    })
+
+}
+
+function salvarTarefas() {
+   const liTarefas = tarefas.querySelectorAll('li')
+   const listaDeTarefas = []
+
+   for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
+    listaDeTarefas.push(tarefaTexto)
+   }
+   const tarefasJSON = JSON.stringify(listaDeTarefas)
+   localStorage.setItem('tarefas', tarefasJSON) 
+}
+
+function addTarefaSalvas() {
+    const tarefas = localStorage.getItem('tarefas')
+    const listaDeTarefas =JSON.parse(tarefas)
+
+    for (let tarefa of listaDeTarefas) {
+        criarTarefa(tarefa)
+    }
+}
+
+btnTarefa.addEventListener('click', function (e) {
+    inputTarefa.value === '' ? alert('digite uma tarefa') : criarTarefa(inputTarefa.value); limpaInput()
+})
+
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        inputTarefa.value === '' ? alert('digite uma tarefa') : criarTarefa(inputTarefa.value), limpaInput()
+    }
+})
+
+addTarefaSalvas()
+apagarTarefa()
