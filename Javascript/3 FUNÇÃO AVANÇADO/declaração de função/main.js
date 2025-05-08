b91ca8237c2c91bebb89a6a1e04b6a61853aa0c6
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-05-08 20:49:05 -0300

    Falando sobre declaração de funções

---
+++ Javascript/3 FUNÇÃO AVANÇADO/declaração de função/main.js
@@ -0,0 +1,29 @@
+// Declaração de função (Function hoisting)
+falaOi()
+
+function falaOi() {
+    console.log('Oii')
+}
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+    console.log('Sou um dado.')
+}
+souUmDado()
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function')
+}
+
+funcaoArrow()
+
+// Dentro de um objeto
+const obj = {
+    falar() {
+console.log('Estou falando...')
+    }
+}
+
+obj.falar()
