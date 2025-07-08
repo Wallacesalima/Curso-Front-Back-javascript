47d92904dc7846ea7a84604c9adbe03ee6ed6267
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-07-07 22:51:13 -0300

    resolvendo desafio

--- 01- RELEMBRANDO/funções/função desafio.js
+++ 01- RELEMBRANDO/funções/função desafio.js
@@ -1,17 +1,37 @@
-function sistemaVotacao(candidatos = []) {
-    return {
-        votar(nome) {
-            for (index in candidatos) {
-                for (this.nome of candidatos) {
-                    if (this.nome === candidatos[index]) {
-                        console.log(nome)
-                    }
-                }
-            }
+function criarListaDeTarefas(callback) {
+    const tarefas = []
+
+    function adicionarTarefas(tarefa) {
+        if (tarefa !== undefined) {
+            tarefas.push(tarefa)
+            callback(`Tarefa adicionada: ${tarefa}`)
+        } else {
+            callback(`Erro ao adicionar tarefa!`)
         }
     }
+
+    function listar() {
+        console.log('Tarefas:', tarefas)
+    }
+
+    function removerTarefas(tarefa) {
+        let indice = tarefas.indexOf(tarefa)
+        if (indice !== -1) {
+            tarefas.splice(indice, 1)
+            callback(`Tarefa removida: ${tarefa}`);
+        } else {
+            callback(`Tarefa não encontrada: ${tarefa}`);
+        }
+
+    }
+    return { adicionarTarefas, listar, removerTarefas }
 }
 
-const sistema = sistemaVotacao(['Ana', 'João', 'Maria']);
+const lista = criarListaDeTarefas((msg) => console.log(`[LOG]: ${msg}`));
 
-sistema.votar('Ana')
+lista.adicionarTarefas("Estudar funções");
+lista.adicionarTarefas("Praticar DOM");
+lista.listar();
+
+lista.removerTarefas("Estudar funções");
+lista.listar();
