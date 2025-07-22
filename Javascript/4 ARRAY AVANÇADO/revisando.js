a59676d3e8e9e6a63f7076b8c1a1dd749c432104
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-07-21 22:28:42 -0300

    Começando nova etapa

---
+++ Javascript/4 ARRAY AVANÇADO/revisando.js
@@ -0,0 +1,10 @@
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+// const novo = nomes.slice(1, -2)
+// console.log(novo)
+
+// const nome = 'Luiz Otávio Miranda'
+// const nomes = nome.split(' ')
+// console.log(nomes)
+const nomes = [ 'Luiz', 'Otávio', 'Miranda' ]
+const nome = nomes.join(' ')
+console.log(nome)
