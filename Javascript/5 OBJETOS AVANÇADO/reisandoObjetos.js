bc87d0a61301736586d9db327acbf0eac6947791
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2025-09-08 22:18:01 -0300

    Começando nova sessão - Objetos avançado

---
+++ Javascript/5 OBJETOS AVANÇADO/reisandoObjetos.js
@@ -0,0 +1,14 @@
+// Factory functions
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        }
+    }
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio')
+console.log(p1.nomeCompleto)
+
